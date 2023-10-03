c3f3281de89144aa0577f09de1b06717cf98d855
Aditya Kharbanda <adityakharbanda25@gmail.com>
2023-10-03 19:26:07 +0100

    add_node and print_list functions added

--- Linked_List_Assignment/single_ll.c
+++ Linked_List_Assignment/single_ll.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct node
 {
     char c;
     struct node* next;
 } Node;
+
+void add_node(Node** list, char data)
+{
+    Node* new = (Node*)malloc(sizeof(Node));
+    if(new == NULL)
+    {
+        printf("Couldn't create Node\n");
+    }
+    new->next = (*list);
+    new->c = data;
+    *list = new;
+}
+
+void print_list(Node* list)
+{
+    Node* temp = list;
+    while(temp != NULL)
+    {
+        if (temp->next == NULL)
+        {
+            printf("%c->NULL", temp->c);
+            break;
+        }
+        printf("%c->", temp->c);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+int main(void)
+{
+    Node* head = NULL;
+    add_node(&head, 'A');
+    add_node(&head, 'Y');
+    add_node(&head, 'T');
+    add_node(&head, 'I');
+    add_node(&head, 'D');
+    add_node(&head, 'A');
+    print_list(head);
+}
